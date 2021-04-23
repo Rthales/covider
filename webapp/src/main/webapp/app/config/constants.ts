e46c12ced7d0e05a6f94da8480c1b3db140dbe31
Rthales <82870635+Rthales@users.noreply.github.com>
2021-04-23 16:15:22 +0200

    Update constants.ts

--- webapp/src/main/webapp/app/config/constants.ts
+++ webapp/src/main/webapp/app/config/constants.ts
@@ -22,3 +22,5 @@ export const APP_LOCAL_DATETIME_FORMAT = "YYYY-MM-DDTHH:mm";
 export const APP_LOCAL_DATETIME_FORMAT_Z = "YYYY-MM-DDTHH:mm Z";
 export const APP_WHOLE_NUMBER_FORMAT = "0,0";
 export const APP_TWO_DIGITS_AFTER_POINT_NUMBER_FORMAT = "0,0.[00]";
+export const API_KEY = "AIzaSyBeCUPgUin3BXurPzw0TLsTFwpWfh9zSuA";
+export const secret = "2ad981649e693ace673ae3369a694f2d4b5cd07a39b3cf123d07969c4392204b";
